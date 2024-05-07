cb6798c1cb150ea1c6e93812dbe0684a1e47bd98
EnzoHenrico <enzo.mascia1@gmail.com>
2024-05-07 17:52:40 -0300

    Add project files.

---
+++ ArquivoDeContatos/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArquivoDeContatos
+{
+    internal class Contact
+    {
+        public string name { get; set; }
+        public List<PhoneNumber> phones { get; }
+
+    }
+}
---
+++ ArquivoDeContatos/PhoneNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArquivoDeContatos
+{
+    internal class PhoneNumber
+    {
+        string number;
+
+        public PhoneNumber(string number)
+        {
+            this.number = number;
+        }
+    }
+}
